1970370cf947aa29a724f1a9c14c89afbcedec52
C80609A <c080609a@gmail.com>
2018-06-18 12:35:23 +0300

    Поправил конфиг spec/rails_helper.rb.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,8 +20,7 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configure do |config|
-  config.include FactoryBot::Syntax::Methods
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!

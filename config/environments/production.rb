2664028f37d02bfb7042bbbfb342089323a58bb2
C80609A <c080609a@gmail.com>
2018-06-18 12:32:11 +0300

    Поправил config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Dir[Rails.root.join('app/controllers/concerns/**/*.rb')].each { |f| require f }
 end
